899e64de786cba76bec2462443bb62c264d87278
Dmitry Meyerson <meyerson@physics.utexas.edu>
2012-06-10 23:41:17 -0500

    some helimak files

--- examples/Helimak/runcase.sh
+++ examples/Helimak/runcase.sh
@@ -27,30 +27,54 @@ make
 echo Running with NP = $NP       
 
 rm -rf data*
-cp BOUT_relax.inp BOUT.inp
+#cp BOUT_relax.inp BOUT.inp
 
 #llist=(0.10 -0.10 )
-llist=(0.10)
 
+current_dir=$PWD
+data_dir='/tmp/hmlk'
+
+
+llist=(0.10)
+rm status.log
+i=0
 for lval in ${llist[@]}
 do
   mkdir data_${lval}
   ln -s data_${lval} data
   
-  cp hlmk.cxx data/2fluid.cxx.ref
-  cp hlmk.cxx data/hlmk.cxx.ref #copy the source code for easy reference
+  current_dir=$data_dir/data_short_${lval}
+  echo $current_dir
+    
+  rm -r $current_dir
+  mkdir -p $current_dir
+  
+  rm -r $PWD/data_${lval}
+
+  cp hlmk.cxx   $current_dir/2fluid.cxx.ref
+  cp hlmk.cxx   $current_dir/hlmk.cxx.ref
+
+  cp hlmk.cxx   $PWD/data_${lval}/hlmk.cxx.ref
+
+#  cp hlmk.cxx data/2fluid.cxx.ref
+ # cp hlmk.cxx data/hlmk.cxx.ref #copy the source code for easy reference
   #cp  data_${lval}/* data #copy the simulation data from the first part of the run
   
   #sed "s/..\/Helimak\/Ln_lam\/Helimak_32x32_0.10_lam_n.nc/..\/Helimak\/Ln_lam\/Helimak_32x32_${lval}_lam_n.nc/g" BOUT.inp > data/BOUT.inp
-  sed "s/Helimak_4x32_0.10_lam_n.nc/Helimak_4x32_${lval}_lam_n.nc/g" BOUT.inp > data/BOUT.inp
+  #sed "s/Helimak_4x32_0.10_lam_n.nc/Helimak_4x32_${lval}_lam_n.nc/g" BOUT.inp >  $current_dir/BOUT.inp
   #if [ $zval -lt 128 ]
       #then
       # reduce time-step. At large times these cases produce noise
       #sed "s/TIMESTEP = 5e3/TIMESTEP = 1e3/g" data/BOUT.inp > data/tmp
       #mv -f data/tmp data/BOUT.inp
   #fi
-      
-  $MPIEXEC $NP ./hlmk
+  cp BOUT.inp $current_dir/BOUT.inp
+  echo "$((i++))"  
+  
+  $MPIEXEC $NP ./hlmk -d $current_dir
+
+  ln -s $current_dir $PWD/data_${lval}
+  echo $current_dir >> status.log
   #ibrun -n $NP -o 0  ./2fluid 
   #wait
   rm -f data
